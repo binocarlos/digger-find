dc8d6918b265d38e4aa58f54fccba90d76008a36
Kai Davenport <kaiyadavenport@gmail.com>
2013-07-26 16:34:20 +0000

    tests passing

---
+++ src/search.js
@@ -0,0 +1,205 @@
+/*
+
+	(The MIT License)
+
+	Copyright (C) 2005-2013 Kai Davenport
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ */
+
+/*
+  Module dependencies.
+*/
+
+
+var utils = require('digger-utils');
+
+/*
+  Quarry.io - Container Search
+  ----------------------------
+
+  Takes a search_from container and an array of strings
+
+  It reverses the strings and does a simulated pipe
+
+
+ */
+
+module.exports.searcher = search;
+module.exports.compiler = compile;
+
+/*
+  These functions are used to run attribute selectors over in-memory containers
+ */
+var attr_compare_functions = {
+  "=":function(check, target){
+    return check!=null && check==target;
+  },
+  "!=":function(check, target){
+    return check!=null && check!=target;
+  },
+  ">":function(check, target){
+    target = parseFloat(target);
+    return check!=null && !isNaN(target) ? check > target : false;
+  },
+  ">=":function(check, target){
+    target = parseFloat(target);
+    return check!=null && !isNaN(target) ? check >= target : false;
+  },
+  "<":function(check, target){
+    target = parseFloat(target);
+    return check!=null && !isNaN(target) ? check < target : false;
+  },
+  "<=":function(check, target){
+    target = parseFloat(target);
+    return check!=null && !isNaN(target) ? check <= target : false;
+  },
+  "^=":function(check, target){
+    return check!=null && check.match(new RegExp('^' + utils.escapeRegexp(target), 'i')) !== null;
+  },
+  "$=":function(check, target){
+    return check!=null && check.match(new RegExp(utils.escapeRegexp(target) + '$', 'i')) !== null;
+  },
+  "~=":function(check, target){
+    return check!=null && check.match(new RegExp('\\W' + utils.escapeRegexp(target) + '\\W', 'i')) !== null;
+  },
+  "|=":function(check, target){
+    return check!=null && check.match(new RegExp('^' + utils.escapeRegexp(target) + '-', 'i')) !== null;
+  },
+  "*=":function(check, target){
+    return check!=null && check.match(new RegExp(utils.escapeRegexp(target), 'i')) !== null;
+  }
+}
+
+/*
+
+  Turn a selector object into a compiled function to have containers run through
+  
+ */
+
+function compile(selector){
+
+  // return a function that will return boolean for a container matching this selector
+  return function(container){
+
+    var model = container.get(0);
+    var digger = model._digger;
+
+    // we step through one at a time - as soon as something fails we do not match
+
+    // if we have a wildcard then we pass
+    if(selector.tag=='*'){
+      return true;
+    }
+
+    // #id
+    if(selector.id && digger.id!=selector.id){      
+      return false;
+    }
+
+    // =diggerid
+    if(selector.diggerid && digger.diggerid!=selector.diggerid){
+      return false;
+    }
+
+    // tagname
+    if(selector.tag && digger.tag!=selector.tag){
+      return false;
+    }
+  
+    // classnames
+    if(selector.class){
+      var keys = Object.keys(selector.class || {});
+      var classcount = 0;
+      keys.forEach(function(c){
+        classcount += container.hasClass(c) ? 1 : 0;
+      })
+      if(classcount<keys.length){
+        return false;
+      }
+    }
+    
+    if(selector.attr){
+
+      var attr_count = 0;
+
+      selector.attr.forEach(function(attr_filter){
+
+        var check_value = container.attr(attr_filter.field);
+        var operator_function = attr_compare_functions[attr_filter.operator];
+
+        // [size]
+        if(!attr_filter.value){
+          attr_count += check_value !== null ? 1 : 0;
+        }
+        // [size>100]
+        else if(operator_function){
+          attr_count += operator_function.apply(null, [check_value, attr_filter.value]) ? 1 : 0;
+        }
+        // no operator function found
+      })
+
+      if(attr_count<selector.attr.length){
+        return false;
+      }
+    }
+
+    return true;
+      
+  }
+}
+
+function search(selector, context){
+
+  var selector_filter = compile(selector);
+
+  var search_in = context;
+
+  // we must now turn the search_from container into a flat array of the things to actually search
+
+  // direct child mode
+  if(selector.splitter=='>'){
+    search_in = context.children();
+  }
+  // direct parent mode
+  else if(selector.splitter=='<'){
+    throw new Error('we do not support the parent splitter at the moment');
+  }
+  // all descendents mode
+  else{
+    search_in = context.descendents();
+  }
+
+  // now we loop each child container piping it via the selector filter
+  var ret = search_in.filter(selector_filter);
+
+  var modifier = selector.modifier || {};
+
+  if(modifier.limit){
+    var st = '' + modifier.limit;
+    if(st.indexOf(',')>=0){
+      var parts = st.split(',').map(function(stt){
+        return stt.replace(/\D/g, '');
+      })
+      ret.models = ret.models.slice(parts[0], parts[1]);
+    }
+    else{
+      ret.models = ret.models.slice(0, modifier.limit);
+    }
+    
+  }
+
+  if(modifier.first && ret.models.length>0){
+    ret.models = [ret.models[0]];
+  }
+  else if(modifier.last && ret.models.length>0){
+    ret.models = [ret.models[ret.models.length-1]];
+  }
+
+  return ret;
+}
---
+++ test/find.test.js
@@ -0,0 +1,42 @@
+var api = require('../src')
+var Container = require('digger-container');
+var citydata = require('./fixtures/cities.json');
+
+Container.augment_prototype(api);
+
+describe('digger-find', function(){
+
+  it('should run selectors on local data', function() {
+
+    var test = Container(citydata);
+
+    var results = test.find('city.south');
+    results.count().should.equal(3);
+
+    var results2 = test.find('country[name^=U] > city.south area.poor');
+    results2.count().should.equal(3);
+
+  })
+  
+  it('should apply limit and first and last modifiers', function() {
+    var test = Container(citydata);
+    test.find('city.south').count().should.equal(3);    
+    test.find('city.south:first').count().should.equal(1);
+    test.find('city.south:last').count().should.equal(1);
+    test.find('city.south:limit(2)').count().should.equal(2);
+  })
+
+  it('should extract a meta skeleton', function() {
+
+    var test = Container(citydata);
+
+    var cities = test.find('city.south');
+
+    var skeleton = cities.skeleton();
+
+    skeleton.length.should.equal(3);
+    skeleton[0].tag.should.equal('city');
+    
+  })
+
+})
